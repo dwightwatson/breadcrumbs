16985d0216ceb0218173519dec8580de6ca0b904
ShibuyaKosuke <shibuya@imagenious.co.jp>
2020-09-02 11:31:21 +0900

    fix typo

--- src/config/breadcrumbs.php
+++ src/config/breadcrumbs.php
@@ -7,7 +7,7 @@ return [
     | View file
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the view to use for rendering the breadrrumbs.
+    | Here you may specify the view to use for rendering the breadcrumbs.
     | By default we use a Bootstrap 4 view, but also support Bootstrap 3.
     |
     */
